221b038dde6c5c2cf63a95ce30a2506ab8971151
armopmsi <razahmadrsofficial@gmail.com>
2024-02-27 17:26:31 +0530

    update

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 function generatePassword() {
     const length = 12;
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&";
+    const charset = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&";
     // const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]|:;<>,.?/~";
     let password = "";
     for (let i = 0; i < length; ++i) {
